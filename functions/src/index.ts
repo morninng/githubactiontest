506fdc81573c9f313d1bc35afec99a795b87b8e0
morninng <mr_morninng_star_dash@yahoo.co.jp>
2022-04-29 18:32:32 +0900

    use secret key file

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,11 @@
 import * as functions from 'firebase-functions';
 
+import { PROJECT_SECRET } from '../secret/test-secret';
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase! bb");
+  const TWITTER_API_KEY = PROJECT_SECRET["mixidea-temp-staging"].TWITTER_API_KEY
+ response.send(TWITTER_API_KEY);
 });
