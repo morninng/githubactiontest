9e35dd19284c85215f046805a2f5252dc41dd9f0
morninng <mr_morninng_star_dash@yahoo.co.jp>
2022-04-30 19:35:14 +0900

    ss

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,14 @@
 import * as functions from 'firebase-functions';
 
-import { PROJECT_SECRET } from '../secret/test-secret';
+// import { PROJECT_SECRET } from '../secret/test-secret';
 import secretJson from '../secret/test-secret2.json';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
-  const TWITTER_API_KEY = PROJECT_SECRET["mixidea-temp-staging"].TWITTER_API_KEY
- response.send(TWITTER_API_KEY);
+  // const TWITTER_API_KEY = PROJECT_SECRET["mixidea-temp-staging"].TWITTER_API_KEY
+ response.send("ss");
 });
 
 export const helloWorld2 = functions.https.onRequest((request, response) => {
